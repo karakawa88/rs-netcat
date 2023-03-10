8c2a819d23189946d19acf6e6b666cffe4a33d69
kagalpan <kagalpan@kacpp.xyz>
2023-03-10 12:15:39 +0900

    dotenvで環境変数ファイルの設定追加。
    clapと連携できるかテストも追加。

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
 use clap::ArgGroup;
+use dotenv::dotenv;
+use std::path::Path;
+use home_dir::*;
 
 ///netcatの動作モード。
 ///TCP・UDPサーバー・クライアントとポートスキャンなどがある。
@@ -19,7 +22,7 @@ enum NcMode {
 #[derive(Debug, Parser)]
 #[command(group(ArgGroup::new("protocol").required(false).args(["tcp", "udp"])))]
 pub struct Cli {
-    #[arg(name="tcp", short='t', long="tcp", help="TCPクライアント・サーバー", env="RSNC_TCP", action = clap::ArgAction::SetFalse)]
+    #[arg(name="tcp", short='t', long="tcp", help="TCPクライアント・サーバー", env="RSNC_TCP")]
     tcp: bool,
     #[arg(name="udp", short='u', long="udp", env="RSNC_UDP", help="UDPクライアント・サーバー")]
     udp: bool,
@@ -34,6 +37,22 @@ pub struct Cli {
 
 }
 
+/// ユーザーの$HOMEの環境変数ファイル
+const HOME_DOTENV_FNAME: &str = ".rsncenv"; 
+/// dotenvの設定
+/// 環境変数ファイルは$HOMEの`HOME_DOTENV_FNAME`とカレントディレクトリの.envファイル。
+/// #Example
+/// set_dotenv();
+///
+fn set_dotenv() {
+    let path = Path::new(HOME_DOTENV_FNAME).expand_home().unwrap();
+    if path.exists() {
+        dotenv::from_path(path);
+    } else {
+        dotenv().ok();
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -43,7 +62,7 @@ mod test {
     fn test_cli_default() {
         let args = Cli::try_parse_from([""]);
         let args = args.unwrap();
-        assert_eq!(true, args.tcp);
+        assert_eq!(false, args.tcp);
         assert_eq!(false, args.udp);
         assert_eq!(None, args.port);
         assert_eq!(None, args.ipaddr);
@@ -52,8 +71,21 @@ mod test {
     fn test_cli() {
         let args = Cli::try_parse_from(["", "-u", "-l", "54321", "127.0.0.1"]);
         let args = args.unwrap();
-        assert_eq!(true, args.tcp);
+        assert_eq!(false, args.tcp);
         assert_eq!(true, args.udp);
+        assert_eq!(54321, args.port.unwrap());
+        assert_eq!("127.0.0.1".to_string(), args.ipaddr.unwrap());
+        
+    }
+
+    #[test]
+    fn test_dotenv() {
+        set_dotenv();
+        let args = Cli::try_parse_from([""]);
+        let args = args.unwrap();
+        assert_eq!(true, args.tcp);
+        assert_eq!(false, args.udp);
+        assert_eq!(true, args.listen);
         assert_eq!(54321, args.port.unwrap());
         assert_eq!("127.0.0.1".to_string(), args.ipaddr.unwrap());
         
